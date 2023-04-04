5a6aa8004cbac633bb3698745215b48ac31ba156
Victor Muchiaroni <47416373+vimuchiaroni@users.noreply.github.com>
2023-04-04 17:43:43 -0300

    Update main_linkerd.tf
    Fix certificate expiration to 10years

--- module/main_linkerd.tf
+++ module/main_linkerd.tf
@@ -44,7 +44,7 @@ resource "tls_locally_signed_cert" "issuer_cert" {
   ca_key_algorithm      = tls_private_key.trustanchor_key[count.index].algorithm
   ca_private_key_pem    = tls_private_key.trustanchor_key[count.index].private_key_pem
   ca_cert_pem           = tls_self_signed_cert.trustanchor_cert[count.index].cert_pem
-  validity_period_hours = 8760
+  validity_period_hours = 87600
   is_ca_certificate     = true
 
   allowed_uses = [
